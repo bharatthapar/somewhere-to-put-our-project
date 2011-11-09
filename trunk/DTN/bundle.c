336b44b8ef158289ded05e14b03f3b7db18dfda8
ddstyles <ddstyles@53a455a5-a154-c692-71da-05f8f862cae6>
2011-11-09 23:44:56 +0000

    Bundle layer

---
+++ trunk/DTN/bundle.c
@@ -0,0 +1,11 @@
+#include "bundle.h"
+
+packet * deliverPacket(packet * p) {
+
+
+}
+
+void newPacket(char dest[4], char * data, int len) {
+
+
+}
---
+++ trunk/DTN/bundle.h
@@ -0,0 +1,9 @@
+#ifndef BUNDLE_H
+#define BUDLE_H
+
+#include "packet.h"
+
+packet * deliverPacket(packet * p);
+
+
+#endif
