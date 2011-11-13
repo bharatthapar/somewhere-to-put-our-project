b4a2173314f2d3f5c72e602ff2a28433f74bd193
ddstyles@gmail.com <ddstyles@gmail.com@53a455a5-a154-c692-71da-05f8f862cae6>
2011-11-13 06:48:35 +0000

    Fixed seg fault in linked list in queuestruc, moved main to bundle, added crude chat client.

--- DTN/UDP_server.c
+++ DTN/UDP_server.c
@@ -27,7 +27,29 @@ int MainSocket;
 int broadcast = 1;
 struct sockaddr_in server_addr , client_addr;
 char *my_ip[4],*temp;
-char ipp[4];
+
+
+void * oldMain() {
+	struct itimerval it_val;	/* for setting itimer */
+    pthread_t sender,receiver;
+	//pthread_create(&sender,NULL,sendPackets,NULL);
+	pthread_create(&receiver,NULL,waitForPacket,NULL);
+	if (signal(SIGALRM, (void (*)(int)) send_beacon) == SIG_ERR) {
+    			perror("Unable to catch SIGALRM");
+    			exit(1);
+  	}
+  	it_val.it_value.tv_sec =     INTERVAL/1000;
+  	it_val.it_value.tv_usec =    (INTERVAL*1000) % 1000000;	
+  	it_val.it_interval = it_val.it_value;
+  	if (setitimer(ITIMER_REAL, &it_val, NULL) == -1) {
+  		  perror("error calling setitimer()");
+  		  exit(1);
+  	}
+
+ 	while (1) 
+   		pause();
+	return 0;
+}
 
 void get_my_ip() {
 	/* int fd;
@@ -46,7 +68,7 @@ void get_my_ip() {
 	
 //	 close(fd);
 //	 char *temp=inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr);
-	//printf("\n\n\n%s\n\n\n",my_ip);
+	////printf("\n\n\n%s\n\n\n",my_ip);
 	//strcpy(my_ip,temp);
 //	sscanf(temp,my_ip
 	//inet_aton(temp,(struct in_addr*)my_ip); 
@@ -69,7 +91,7 @@ void get_my_ip() {
 	if (my_ip[3]<0)
 		my_ip[3]=my_ip[3]+256;
 
-printf("IP is %d.%d.%d.%d\n",my_ip[0],my_ip[1],my_ip[2],my_ip[3]);
+//printf("IP is %d.%d.%d.%d\n",my_ip[0],my_ip[1],my_ip[2],my_ip[3]);
 */
 char hostbuf[256];
        // char * ipbuf;
@@ -97,7 +119,7 @@ char hostbuf[256];
                 exit(1);
         }
 	int i =0;
-	//printf("%s\n",temp);
+	////printf("%s\n",temp);
 	my_ip[0] = (char*)strtok(temp,".");
 		
 		while (my_ip[i])
@@ -105,14 +127,14 @@ char hostbuf[256];
 			 i++;
 	  		 my_ip[i] = strtok (NULL, ".");
 			//itoa(my_ip[i],my_ip[i][0],10);
-			//printf("%s.....",my_ip[i]);
+			////printf("%s.....",my_ip[i]);
 	  	}
 		
 	my_ip[0]=atoi(my_ip[0]);
 	my_ip[1]=atoi(my_ip[1]);
 my_ip[2]=atoi(my_ip[2]);
 my_ip[3]=atoi(my_ip[3]);
-//	printf("IP is %d.%d.%d.%d",my_ip[0],my_ip[1],my_ip[2],my_ip[3]);
+	//printf("IP is %d.%d.%d.%d",my_ip[0],my_ip[1],my_ip[2],my_ip[3]);
 	
 	
 	ipp[0] = (int)my_ip[0];
@@ -120,7 +142,7 @@ my_ip[3]=atoi(my_ip[3]);
 	ipp[2] = (int)my_ip[2];
 	ipp[3] = (int)my_ip[3];
 
-    //    printf("Hostname: %s Host IP: %s\n", hostbuf, my_ip);
+    //    //printf("Hostname: %s Host IP: %s\n", hostbuf, my_ip);
 
 
 
@@ -134,10 +156,10 @@ int initialize() {
 		char host[100];
 		gethostname(host,sizeof(host));	
 		
-		//printf("Host is %s\n\n",host);	
+		////printf("Host is %s\n\n",host);	
 		struct hostent *name=gethostbyname(host);
 		get_my_ip();
-		//printf("IP is %s",my_ip[0]);
+		////printf("IP is %s",my_ip[0]);
 		MainSocket = socket(AF_INET,SOCK_DGRAM,0);
 		if(MainSocket == -1) {
 			perror("Socket");
@@ -154,8 +176,15 @@ int initialize() {
         	server_addr.sin_port = htons(8000);
         	server_addr.sin_addr.s_addr = INADDR_ANY;
         	bzero(&(server_addr.sin_zero),8);
+			
+		pthread_t run;
+		pthread_create(&run,NULL,oldMain,NULL);
+	
 		return MainSocket;
 }
+
+
+
 
 void *waitForPacket() {
 		struct Apacket *packet1;
@@ -175,16 +204,17 @@ void *waitForPacket() {
 	        fflush(stdout);
 		while(1) {
 			
+			
 			bytes_read = recvfrom(listen_sock,packet1,1024,0,(struct sockaddr *)&client_addr, &addr_len);
-	  		
+
 			if(packet1->source[0]!=(my_ip[0]-256)||packet1->source[1]!=(my_ip[1]-256)||packet1->source[2]!=my_ip[2]||packet1->source[3]!=(my_ip[3]-256))
-			{	//printf("\n\n\n%s\n\n\n",my_ip);
-				//printf("\n\n%d.%d.%d.%d\n\n",packet1->source[0],packet1->source[1],packet1->source[2],packet1->source[3]);
-	          //	printf("\n(%s , %d) said : ",inet_ntoa(client_addr.sin_addr),ntohs(client_addr.sin_port));
-	          //	printf("\n%s\n", packet1->data);
+			{	////printf("\n\n\n%s\n\n\n",my_ip);
+				////printf("\n\n%d.%d.%d.%d\n\n",packet1->source[0],packet1->source[1],packet1->source[2],packet1->source[3]);
+	          //	//printf("\n(%s , %d) said : ",inet_ntoa(client_addr.sin_addr),ntohs(client_addr.sin_port));
+	          //	//printf("\n%s\n", packet1->data);
 
 			if(packet1->type == TYPE_BEACON) {
-				//printf("Received beacon from %s, sending all packets\n",inet_ntoa(client_addr.sin_addr));
+				////printf("Received beacon from %s, sending all packets\n",inet_ntoa(client_addr.sin_addr));
 				send_all(inet_ntoa(client_addr.sin_addr));
 			}
 			else if(packet1->type == TYPE_DATA) {
@@ -204,56 +234,31 @@ void sendPackets(struct Apacket *packet, char *ip) {
 		server_addr.sin_addr.s_addr = inet_addr(ip);
 		server_addr.sin_port = htons(8000);
 		bzero(&(server_addr.sin_zero),8);
-		//printf("Sending to %s on port %d\n",inet_ntoa(server_addr.sin_addr),ntohs(server_addr.sin_port));
-		//printf("Data sent is %s with sequence number %d\n",packet->data,packet->seq_num);
+		////printf("Sending to %s on port %d\n",inet_ntoa(server_addr.sin_addr),ntohs(server_addr.sin_port));
+		////printf("Data sent is %s with sequence number %d\n",packet->data,packet->seq_num);
 		sendto(send_sock, (char *)packet, packet->length, 0,(struct sockaddr *)&server_addr, sizeof(struct sockaddr));
-		//printf("bhej diya\n");
+		////printf("bhej diya\n");
 }
 
-int main(int argc, char *argv[])
-{
-	struct itimerval it_val;	/* for setting itimer */
-    	initialize();
-	pthread_t sender,receiver;
-	//pthread_create(&sender,NULL,sendPackets,NULL);
-	pthread_create(&receiver,NULL,waitForPacket,NULL);
-	if (signal(SIGALRM, (void (*)(int)) send_beacon) == SIG_ERR) {
-    			perror("Unable to catch SIGALRM");
-    			exit(1);
-  	}
-  	it_val.it_value.tv_sec =     INTERVAL/1000;
-  	it_val.it_value.tv_usec =    (INTERVAL*1000) % 1000000;	
-  	it_val.it_interval = it_val.it_value;
-  	if (setitimer(ITIMER_REAL, &it_val, NULL) == -1) {
-  		  perror("error calling setitimer()");
-  		  exit(1);
-  	}
 
- 	while (1) 
-   		pause();
-	return 0;
-}
 
 void send_beacon() {
-	//printf("Im in\n");
+	////printf("Im in\n");
 	struct Apacket packet;
 	packet.type = 1;
 	packet.seq_num = 0;
 	packet .ttl = 2;
 	sprintf(packet.data,"Beacon\n");
-	//printf("%s\n",packet.data);
-	//printf("IP is %d.%d.%d.%d\n",my_ip[0],my_ip[1],my_ip[2],my_ip[3]);	
+	////printf("%s\n",packet.data);
+	////printf("IP is %d.%d.%d.%d\n",my_ip[0],my_ip[1],my_ip[2],my_ip[3]);	
 	packet.source[0]=my_ip[0];
 	packet.source[1]=my_ip[1];
 	packet.source[2]=my_ip[2];
 	packet.source[3]=my_ip[3];
-	//printf("%d.%d.%d.%d\n",packet.source[0]+256,packet.source[1]+256,packet.source[2],packet.source[3]);
+	////printf("%d.%d.%d.%d\n",packet.source[0]+256,packet.source[1]+256,packet.source[2],packet.source[3]);
 	packet.length = sizeof(packet)-MAX_FRAME_SIZE+strlen(packet.data);
-	char dest[4]={192,168,1,125};
+	char dest[4]={192,168,1,105};
 	sendPackets(&packet,"192.168.1.255");
-	
-	newPacket(ipp,dest);
-	
 }
 
 void data_handler(struct Apacket *packet) {
@@ -261,7 +266,7 @@ void data_handler(struct Apacket *packet) {
 	struct Apacket *ack;
 	if(isOld(packet)==NOT_OLD_PACKET) {
 		if(!memcmp(packet->dest,ipp,4))	//The received packet is destined for me.
-		{	//printf("Hello Hello");	
+		{	////printf("Hello Hello");	
 			ack=deliverPacket(packet);	//Get the ACK from the bundle layer
 			isOld(ack);
 			add_packetnode(ack);
@@ -279,7 +284,7 @@ void ack_handler(struct Apacket *packet) {
 	/*Got ACK ---- Update seq num ---- If 1 -> store ACK in linked list....remove all old data and ACK packets, else return0*/
 
 	if(isOld(packet)==NOT_OLD_PACKET) {
-		printf("\n\nGOT AK FROM DAN\n\n");
+		//printf("\n\nGOT AK FROM DAN\n\n");
 		add_packetnode(packet);
                 delete_packetnode(packet);
 		print_all();
